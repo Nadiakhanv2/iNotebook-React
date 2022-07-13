f1da224f3b2ceda3919f4c55a67dda70d6013601
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-07-13 23:05:15 +0500

    import data validation from express js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const {body, validationResult}=require('express-validator');
 const User= require('../models/User');
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.post("/", [
+    
+],(req, res) => {
     // Create a user using : POST "/api/auth/". Doesn't require authentication.
     const user = User(req.body);
-    user.save()
+    user.save();
     
     console.log(req.body);
     res.send(req.body);
