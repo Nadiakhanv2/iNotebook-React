6eb64623b38e304787592bb58f98cb106e09527a
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-07-13 23:08:46 +0500

    array data

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,9 @@ const User= require('../models/User');
 const router = express.Router();
 
 router.post("/", [
-    
+    body('name').isLength({ min: 3 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 6 })
 ],(req, res) => {
     // Create a user using : POST "/api/auth/". Doesn't require authentication.
     const user = User(req.body);
