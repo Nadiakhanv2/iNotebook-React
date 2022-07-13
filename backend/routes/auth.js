73d51cc89fe440bd70b599e9d63a03078aa1ba3f
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-07-13 23:17:23 +0500

    error validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,13 +4,15 @@ const User= require('../models/User');
 const router = express.Router();
 
 router.post("/", [
-    body('name').isLength({ min: 3 }),
-    body('email').isEmail(),
-    body('password').isLength({ min: 6 })
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password' , 'Password must be at least 5 characters').isLength({ min: 6 })
 ],(req, res) => {
     // Create a user using : POST "/api/auth/". Doesn't require authentication.
-    const user = User(req.body);
-    user.save();
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     
     console.log(req.body);
     res.send(req.body);
