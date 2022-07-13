b63d3ea1d0af94b8f8f4be337cec3ee8343413bf
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-07-13 22:07:30 +0500

    creating routes and schema for notes in backend folder

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,19 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// Available Routes
+app.use('/api/notes', require('./routes/notes'));
+app.use('/api/auth', require('./routes/auth'));
 
+// app.get("/", (req, res) => {
+//   res.send("Hello Nadia!");
+// });
+// app.get("/api/v1/login", (req, res) => {
+//   res.send("Hello login!");
+// });
+// app.get("/api/v1/signup", (req, res) => {
+//   res.send("Hello signup!");
+// });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
