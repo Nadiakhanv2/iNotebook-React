3f5c8a483f847a1d24c0a9e01d8d44594ab5f188
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-07-16 21:00:44 +0500

    route 3 in notes.js

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -2,6 +2,8 @@ const express = require("express");
 var fetchuser = require("../middleware/fetchuser");
 const Notes = require("../models/Notes");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+
 
     // Route 1 ---Get all the notes using : GET "/api/auth/getuser". Login required
 
@@ -9,5 +11,24 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
   const notes = await Notes.find({ user: req.user.id });
    res.json(notes);
 });
+
+
+// Route 2 ---Add a new note using : POST "/api/auth/addnote". Login required
+
+router.get(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Description must be at least 5 characters").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const notes = await Notes.find({ user: req.user.id });
+    res.json(notes);
+  }
+);
+
 
 module.exports = router;
