61f0decfdf8b2e39d06b272a5cf71d237d6593af
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-07-16 20:54:50 +0500

    .

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,14 +1,13 @@
 const express = require("express");
 var fetchuser = require("../middleware/fetchuser");
 const Notes = require("../models/Notes");
-
 const router = express.Router();
 
     // Route 1 ---Get all the notes using : GET "/api/auth/getuser". Login required
 
-router.get("/fetchallnotes", fetchuser, (req, res) => {
-  
-  res.json([]);
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+  const notes = await Notes.find({ user: req.user.id });
+   res.json(notes);
 });
 
 module.exports = router;
