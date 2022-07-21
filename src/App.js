249bc8a84f862115985504eea410ba7e11f2e408
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-07-21 15:50:01 +0500

    add about component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Home from './components/home';
 
 function App() {
   return (
     <>
-      <Navbar/>
-   <h1>React App</h1>
+      <Navbar />
+      <Home/>
     </>
   );
 }
