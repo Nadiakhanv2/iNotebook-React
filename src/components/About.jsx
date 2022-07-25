5bbd22e41af679659a2d7a39f7b0b1a59af3a65b
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-07-25 15:37:53 +0500

    remove create context

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,17 +2,12 @@ import React, {useContext, useEffect} from 'react'
 import NoteContext from '../context/notes/noteContext'
 
 const About = () => {
-  const a = useContext(NoteContext)
 
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line
-  }, [])
-
+  
 
   return (
     <div>
-      Hey, This is  {a.state.name} and she is in {a.state.class}
+      This is about page
     </div>
   )
 }
