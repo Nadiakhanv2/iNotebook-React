4c61c751fd76c55ae37e3e075033b9c9f44a8e5b
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-07-25 12:39:02 +0500

    useState and update function added

--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -1,4 +1,27 @@
-import React from "react";
-import noteContext from "./NoteContext";
+import React, { useState } from "react";
+import NoteContext from "./noteContext";
 
-const NoteContext=
+const NoteState = (props) => {
+    const state1 = {
+        "name": "Nadya",
+        "class": "B.ed HONS in Education ",
+    }
+    const [state, setState] = useState(state1);
+    const update = () => {
+        setTimeout(() => {
+            setState({
+              "name": "Nads",
+              "class": "B.ed HONS in Computer Science",
+            });
+            
+        }, 1000)
+    }
+    return (
+        <NoteContext.Provider value={{state:state, update:update}}>
+            {props.children}
+        </NoteContext.Provider>
+    );
+    
+}
+
+export default NoteState;
