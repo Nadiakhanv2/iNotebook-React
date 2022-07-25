11e1fb5a73353d950c53c908b60d6308dc0d05ec
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-07-25 15:34:59 +0500

    remove function update

--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -2,22 +2,9 @@ import React, { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-    const state1 = {
-        "name": "Nadya",
-        "class": "B.ed HONS in Education ",
-    }
-    const [state, setState] = useState(state1);
-    const update = () => {
-        setTimeout(() => {
-            setState({
-              "name": "Nads",
-              "class": "B.ed HONS in Computer Science",
-            });
-            
-        }, 1000)
-    }
+   
     return (
-        <NoteContext.Provider value={{state:state, update:update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     );
